114fddc5b844bc85449af9b4cd2dac2114e4977a
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-04-28 21:45:01 +0000

    changes to odd and even functions

--- even.js
+++ even.js
@@ -3,8 +3,8 @@ const isOdd = function(num) {
   return num % 2 === 1;
 }
 
-const tenIsEven = isOdd(10);
-const elevenIsEven = isOdd(11);
+const tenIsOdd = isOdd(10);
+const elevenIsOdd = isOdd(11);
 
 console.log(isOdd(10));
 
